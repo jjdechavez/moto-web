585c91029d3d08401868b6628150ad1ef8c33e15
John Jerald De Chavez <jjdechavez.techmaker@gmail.com>
2020-05-21 10:41:20 +0800

    Add custom pagination and replace status to price field

--- src/components/pages/dashboard/Items.tsx
+++ src/components/pages/dashboard/Items.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useContext } from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useEffect, useContext, useState } from "react";
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,10 +10,14 @@ import Paper from '@material-ui/core/Paper';
 import { ItemContext } from "../../../contexts/dashboard/ItemContext";
 import { getItems, resetItemStatus } from "../../../actions/ItemActions";
 import { iItems } from "../../../interface/Items";
-import { IconButton } from "@material-ui/core";
+import { IconButton, useTheme, TableFooter, TablePagination } from "@material-ui/core";
 import { LinearProgress, withStyles } from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
 const ColorLinearProgress = withStyles({
   colorPrimary: {
@@ -24,16 +28,92 @@ const ColorLinearProgress = withStyles({
   },
 })(LinearProgress);
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 650,
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+        flexShrink: 0,
+        marginLeft: theme.spacing(2.5),
+        },
+        table: {
+            minWidth: 500
+        }
+    }),
+);
+
+// const useStyles = makeStyles({
+//     root: {
+//         flexShrink: 0,
+//         marginLeft: theme.spacing(2.5)
+//     },
+//     table: {
+//         minWidth: 650,
+//     },
+// });
 
 let render = 1;
 
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onChangePage: (event: React.MouseEvent<HTMLButtonElement>, newPage: number) => void;
+}
+
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const classes = useStyles();
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  const handleFirstPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </div>
+  );
+}
+
 const Items = () => {
     const classes = useStyles();
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(5);
     const { itemState, dispatch } = useContext(ItemContext);
     
     const { items, getItemsStatus: { sending, sent, error } } = itemState;
@@ -52,20 +132,55 @@ const Items = () => {
             resetItemStatus(dispatch);
         }
     }, [sent, error, dispatch]);
+    
+
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, items.length - page * rowsPerPage);
+
+    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
     const renderItems = () => {
-        return items.map((item: iItems) => (
+        return (rowsPerPage > 0
+            ? items.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : items
+        ).map((item: iItems) => (
             <TableRow key={item.id}>
-                <TableCell component="th" scope="row">{item.id}</TableCell>
-                <TableCell align="right">{item.name}</TableCell>
-                <TableCell align="right">{item.serialNumber}</TableCell>
-                <TableCell align="right">{item.brand}</TableCell>
-                <TableCell align="right">{item.category}</TableCell>
-                <TableCell align="right">{item.quantity}</TableCell>
-                <TableCell align="right">{item.status}</TableCell>
-                <TableCell align="right">{item.dateAdded}</TableCell>
-                <TableCell align="right">{item.user.firstName}</TableCell>
-                <TableCell align="right">
+                <TableCell component="th" scope="row">
+                    {item.id}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.name}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.serialNumber}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.brand}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.category}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.quantity}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    &#8369; {item.price.toLocaleString()}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.dateAdded}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
+                    {item.user.firstName}
+                </TableCell>
+                <TableCell style={{ width: 160 }} align="right">
                     <IconButton>
                         <EditIcon/>
                     </IconButton>
@@ -74,7 +189,7 @@ const Items = () => {
                     </IconButton>
                 </TableCell>
             </TableRow>
-        ))
+        ));
     }
 
     if (sending) {
@@ -92,7 +207,7 @@ const Items = () => {
                                 <TableCell align="right">Brand</TableCell>
                                 <TableCell align="right">Category</TableCell>
                                 <TableCell align="right">Quantity</TableCell>
-                                <TableCell align="right">Status</TableCell>
+                                <TableCell align="right">Price</TableCell>
                                 <TableCell align="right">Date Added</TableCell>
                                 <TableCell align="right">Created By</TableCell>
                                 <TableCell align="right">Actions</TableCell>
@@ -100,7 +215,30 @@ const Items = () => {
                         </TableHead>
                         <TableBody>
                             {renderItems()}
+                            {emptyRows > 0 && (
+                                <TableRow style={{ height: 53 * emptyRows }}>
+                                    <TableCell colSpan={10} />
+                                </TableRow>
+                            )}
                         </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TablePagination
+                                    rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                                    colSpan={10}
+                                    count={items.length}
+                                    rowsPerPage={rowsPerPage}
+                                    page={page}
+                                    SelectProps={{
+                                        inputProps: { 'aria-label': 'rows per page' },
+                                        native: true,
+                                    }}
+                                    onChangePage={handleChangePage}
+                                    onChangeRowsPerPage={handleChangeRowsPerPage}
+                                    ActionsComponent={TablePaginationActions}
+                                />
+                            </TableRow>
+                        </TableFooter>
                     </Table>
                 </TableContainer>
             </>
